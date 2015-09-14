b3fc40c58ea421304b8860297445f6b7a4af38a3
Louis Jimenez <ljimenez@tps.org>
2015-09-14 11:33:30 -0400

    add git ignore and motor controls

--- test.py
+++ test.py
@@ -1,13 +1,40 @@
 from raspirobotboard import *
-import time
+import sys
 
 rr = RaspiRobot()
 
-while True:
-    rr.set_led1(True)
-    rr.set_led2(True)
-    time.sleep(0.5)
-    rr.set_led1(False)
-    rr.set_led2(False)
-    time.sleep(0.5)
 
+def forward(seconds):
+    rr.set_led1(1)
+    rr.set_led2(1)
+    rr.forward(seconds)
+
+
+def reverse(seconds):
+    rr.set_led1(0)
+    rr.set_led2(0)
+    rr.reverse(seconds)
+
+
+def left(seconds):
+    rr.set_led1(1)
+    rr.set_led2(0)
+    rr.left(seconds)
+
+
+def right(seconds):
+    rr.set_led1(0)
+    rr.set_led2(1)
+    rr.right(seconds)
+
+
+while True:
+    try:
+        forward(2)
+        left(3)
+        right(3)
+        reverse(3)
+    except KeyboardInterrupt:
+        print("Control C Received!")
+        rr.stop()
+        sys.exit(1)
