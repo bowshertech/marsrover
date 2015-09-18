8ed27ced001468147188016c4e75501b4db511a9
Louis Jimenez <ljimenez@tps.org>
2015-09-18 14:17:23 -0400

    keyboard c

--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 from raspirobotboard import *
+from pygame import *
+
 import sys
 
 rr = RaspiRobot()
@@ -27,13 +29,36 @@ def right(seconds):
     rr.set_led2(1)
     rr.right(seconds)
 
+pygame.display.set_caption('RaspiRobot')
+pygame.mouse.set_visible(0)
 
 while True:
     try:
-        forward(2)
-        left(3)
-        right(3)
-        reverse(3)
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                sys.exit()
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    rr.forward()
+                    rr.set_led1(True)
+                    rr.set_led2(True)
+                elif event.key == K_DOWN:
+                    rr.set_led1(True)
+                    rr.set_led2(True)
+                    rr.reverse()
+                elif event.key == K_RIGHT:
+                    rr.set_led1(False)
+                    rr.set_led2(True)
+                    rr.right()
+                elif event.key == K_LEFT:
+                    rr.set_led1(True)
+                    rr.set_led2(False)
+                    rr.left()
+                elif event.key == K_SPACE:
+                    rr.stop()
+                    rr.set_led1(False)
+                    rr.set_led2(False)
+
     except KeyboardInterrupt:
         print("Control C Received!")
         rr.stop()
