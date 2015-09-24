fa4d6e1c75997dc1698473e6276f0b944c5c22bd
Louis Jimenez <ljimenez@tps.org>
2015-09-24 10:44:54 -0400

    adjust LEDS to match direction

--- test.py
+++ test.py
@@ -24,14 +24,14 @@ def reverse():
 
 
 def left():
-    rr.set_led1(1)
-    rr.set_led2(0)
+    rr.set_led1(0)
+    rr.set_led2(1)
     rr.left()
 
 
 def right():
-    rr.set_led1(0)
-    rr.set_led2(1)
+    rr.set_led1(1)
+    rr.set_led2(0)
     rr.right()
 
 while True:
